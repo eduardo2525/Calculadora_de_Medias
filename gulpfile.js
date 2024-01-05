c89e72839ae824fa4f7ec8ec1f7378f73d143ecd
eduardo <marketingdoeduardo@gmail.com>
2024-01-04 22:36:39 -0300

    instalação do plugin sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourceMaps = require('gulp-sourcemaps');
 
 function styles() {
     return gulp.src('./source/styles/*.scss')
+        .pipe(sourceMaps.init())
         .pipe(sass({
             outputStyle: 'compressed'
         }))
+        .pipe(sourceMaps.write('.maps'))
         .pipe(gulp.dest('./build/styles'))
 }
 
