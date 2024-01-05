dff59de96b543b9c5d6d4aaa59135c1dbe01bc1f
eduardo <marketingdoeduardo@gmail.com>
2024-01-04 23:01:23 -0300

    instalação do plugin imagemin e compresão das imagens

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourceMaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./source/styles/*.scss')
@@ -12,8 +13,14 @@ function styles() {
         .pipe(gulp.dest('./build/styles'))
 }
 
-exports.default = gulp.parallel(styles);
+function imagens() {
+    return gulp.src('./source/imagens/*.png')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/imagens'))
+}
+
+exports.default = gulp.parallel(styles, imagens);
 
 exports.watch = function() {
-    gulp.watch('./source/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./source/styles/*.scss', gulp.parallel(styles));
 }
